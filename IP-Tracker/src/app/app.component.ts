7e08011cc99faf6afd9623d9b0be3d638c38e5b1
AdiAlikalfic <128136281+AdiAlikalfic@users.noreply.github.com>
2025-02-18 10:06:36 +0100

    Add files via upload

--- IP-Tracker/src/app/app.component.ts
+++ IP-Tracker/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
-import { DataService } from './services/data.service';
 import { IP } from './models/ip';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -10,29 +10,20 @@ import { IP } from './models/ip';
 })
 export class AppComponent implements OnInit{
   title = 'IP-Tracker';
-  ip: IP[] = []
-  map: any;
 
-  constructor(private dataService: DataService) { };
+  ipAddress : string = '';
+  map: L.Map | undefined;
+  marker: L.Marker | undefined;
+  ipInfo: IP | null = null;
+  
+
+  constructor(private http: HttpClient) { };
 
   ngOnInit(): void {
     this.initMap();
-
-    this.dataService.getIpAddress().subscribe({
-      next: (data: IP[]) => {
-        this.ip = data
-      },
-      error: (error) => {
-        console.log(error);
-        
-      },
-      complete: () => {
-        console.log('complete', this.ip);
-        
-      }
-    }
-    )
   }
+
+
 
   initMap(): void {
     this.map = L.map('map').setView([51.505, -0.09], 13);
@@ -41,5 +32,38 @@ export class AppComponent implements OnInit{
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(this.map);
+  }
+
+  getIpLocation(): void {
+    const apiKey = 'at_tZLOouLSrM8eE0LKYT1ltjucwZCt5'
+    const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${this.ipAddress}`
+
+    this.http.get<any>(url).subscribe(response => {
+      console.log(response);
+      if(response && response.location) {
+        this.ipInfo = response
+        const {lat, lng, city, country} = response.location
+
+        this.showLocationOnMap(lat, lng, city, country);
+        console.log(response);
+        
+      } else {
+        alert('Location not found');
+      }
+    }, error => {
+      console.error('Error fetching IP location: ', error)
+      alert('An error occured while fetching the location')
+    });
+  }
+
+  showLocationOnMap(lat: number, lng: number, city: string, country: string): void {
+    if (this.map) {
+      if (this.marker) {
+        this.marker?.remove()
+      }
+      this.map?.setView([lat, lng], 13);
+      this.marker = L.marker([lat, lng]).addTo(this.map);
+      this.marker.bindPopup(`<b>${city}, ${country}</b>`).openPopup();
+    }
   }
 }
